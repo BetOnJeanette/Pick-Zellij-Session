52d07184a6fa51c6db0c0c048d7650f84fdba248
BetOnJeanette <jeanettecrull99@gmail.com>
2026-02-05 15:12:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this?
+This is a small fish script that loads all of the names of existing Zellij sessions into fzf, and if the user picks one, launches into it
+
+# Setup
+Move pick_multi_session.fish into .config/fish/functions/
+
+# Usage
+Run `pick_multi_session` in fish.
---
+++ pick_multi_session.fish
@@ -0,0 +1,10 @@
+function pick_multi_session
+    set -l sessions_with_br (string match -r ".* \[" $(zellij ls))
+    set -l sessions (string sub -e -2 $sessions_with_br)
+    set -l no_color (string replace -ra '\e\[[^m]*m' '' $sessions)
+    set -l selected $(string join -- \n $no_color | fzf --separator=\n)
+    echo $selected
+    if string length -q $selected
+        zellij a "$selected"
+    end
+end
