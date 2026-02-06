879b2adb1b73e5e1085dc8e0729a2ab24ce5ea1f
BetOnJeanette <jeanettecrull99@gmail.com>
2026-02-06 16:58:09 -0500

    Just use the existing options to get the names and the like, don't worry too much about parsing

--- pick_multi_session.fish
+++ pick_multi_session.fish
@@ -1,8 +1,6 @@
 function pick_multi_session
-    set -l sessions_with_br (string match -r ".* \[" $(zellij ls))
-    set -l sessions (string sub -e -2 $sessions_with_br)
-    set -l no_color (string replace -ra '\e\[[^m]*m' '' $sessions)
-    set -l selected $(string join -- \n $no_color | fzf --separator=\n)
+    set -l sessions $(zellij ls -ns)
+    set -l selected $(string join -- \n $sessions | fzf --separator=\n)
     if string length -q $selected
         zellij a "$selected"
     end
