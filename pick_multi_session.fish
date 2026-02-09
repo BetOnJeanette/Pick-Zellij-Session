f69ea86a36efe9bde8b945fa5b9b81d0548e619b
BetOnJeanette <jeanettecrull99@gmail.com>
2026-02-09 16:10:16 -0500

    Add description

--- pick_multi_session.fish
+++ pick_multi_session.fish
@@ -1,4 +1,4 @@
-function pick_multi_session
+function pick_multi_session -d "Load a zellij session using fzf"
     set -l sessions $(zellij ls -ns)
     set -l selected $(string join -- \n $sessions | fzf --separator=\n)
     if string length -q $selected
